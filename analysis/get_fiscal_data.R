85c09709bdfede8201fe3dd60b6407a6b7a9311e
ricardomayerb <ricardomayerb@gmail.com>
2017-01-17 17:03:14 -0400

    started data munging

--- analysis/get_fiscal_data.R
+++ analysis/get_fiscal_data.R
@@ -2,7 +2,3 @@ library("googlesheets")
 suppressPackageStartupMessages(library("dplyr"))
 
 
-gs_gap() %>% 
-  gs_copy(to = "Gapminder")
-## or, if you don't use pipes
-gs_copy(gs_gap(), to = "Gapminder")
