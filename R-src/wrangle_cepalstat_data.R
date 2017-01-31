2db7eb6d94b4297989af6011157d155845da6cd0
Ricardo Mayer <ricardomayerb@gmail.com>
2017-01-31 00:09:59 -0300

    completar nombre

--- R-src/wrangle_cepalstat_data.R
+++ R-src/wrangle_cepalstat_data.R
@@ -15,7 +15,8 @@ top_16_gdp_countries = c("BRA", "MEX", "ARG", "VEN",
                          "DOM", "GTM", "CRI", "PAN",
                          "URY", "BOL", "PRY", "SLV")
 
-carib_minus_dom = c("ATG", "BHS", "BRB", "DMA", "GRD", "HTI", "KNA", "LCA", "TTO", "VCT")
+carib_minus_dom = c("ATG", "BHS", "BRB", "DMA", "GRD", 
+                    "HTI", "KNA", "LCA", "TTO", "VCT")
 
 other_to_drop = c("GUY", "SUR", "BLZ")
 
@@ -23,8 +24,8 @@ latin_20 = cepal_33_countries %>%
             filter(! iso3c %in% carib_minus_dom) %>% 
             filter(! iso3c %in% other_to_drop)
 
-spanish_to_iso3c= list("Brasil"="BRA", "Republica Dominicana"="DOM")
-iso3c_to_spanish = list("BRA"="Brasil", "DOM"="Republica Dominicana")
+spanish_to_iso3c = list("Brasil" = "BRA", "Republica Dominicana" = "DOM")
+iso3c_to_spanish = list("BRA" = "Brasil", "DOM" = "Republica Dominicana")
 
 foo = c("BRA", "DOM")
 spanish_to_iso3c[["Brasil"]]
