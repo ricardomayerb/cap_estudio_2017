43175a75505d40231cc33fcd1307fd814ba1bc4c
Ricardo Mayer <ricardomayerb@gmail.com>
2017-03-06 15:34:52 -0300

    first joins

--- scripts/create_external_sector_db.R
+++ scripts/create_external_sector_db.R
@@ -18,3 +18,32 @@ load("./produced_data/cs_deuda_externa_20")
 
 # load("./produced_data/bis_gap_tc_dsr")
 load("./produced_data/bis_tidy")
+
+common_variables_a = c("iso2c", "indicador", "rubro", "year", "value",
+                       "file_source")
+
+cs_bp_anual_20_to_join <- cs_bp_anual_20 %>% 
+  mutate(file_source = "cepalstat") %>% 
+  rename(year = Años, rubro = Rubro, value = valor) %>% 
+  select(c(iso2c, indicador, rubro, year, value,
+           file_source))
+
+bop_tidy_20_to_join <- bop_tidy_20 %>% 
+  mutate(file_source = "edd", indicador = rubro) %>% 
+  select(c(iso2c, indicador, rubro, year, value,
+           file_source))
+  
+# bop_dde_cs <- full_join(x = bop_tidy_20, y = cs_bp_anual_20, 
+#                         by = c("iso2c", "file_source", "rubro", "year"),
+#                         suffix = c(".dde", ".cs")) %>% 
+#   arrange(rubro, iso2c, year, file_source )
+
+cs_deuda_externa_to_join <- cs_deuda_externa %>% 
+  rename(year = Años, value = valor) %>% 
+  mutate(rubro = indicador) %>% 
+  mutate(file_source = "edd") %>% 
+  select(c(iso2c, indicador, rubro, year, value,
+           file_source))
+
+
+bop_de_20 <- bind_rows(bop_tidy_20_to_join, cs_deuda_externa_to_join)
